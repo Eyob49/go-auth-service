ce2eb55123173f785d5527dc6b8afaf4836a5635
Eyob <eyobk740@gmail.com>
2026-06-06 00:44:17 +0300

    load jwt secret token

--- cmd/main.go
+++ cmd/main.go
@@ -17,6 +17,10 @@ func main() {
 	if dbURL == "" {
 		log.Fatal("DATABASE_URL environment variable is not set.")
 	}
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		log.Fatal("JWT_SECRET environment variable is not set.")
+	}
 	// Open the pool structures
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
