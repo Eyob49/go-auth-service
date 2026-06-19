a71b39ec528c334735bba4228525e98f02415947
Eyob <eyobk740@gmail.com>
2026-06-20 00:43:40 +0300

    fix: server startup

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"auth/internal/database"
+	"auth/internal/handlers"
+	"net/http"
+	"log"
+)
+
+func main() {
+	db, err := database.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = database.InitDB(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := &handlers.AuthHandler{DB: db}
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /register", handler.Register)
+
+	log.Printf("Server is running on http://localhost:8080")
+	err = http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+
+}
