3a630388b22f0c8ee4c9283e1a366fb555c1e23a
Eyob <eyobk740@gmail.com>
2026-06-19 22:08:07 +0300

    add PostgreSQL DB connection and user table intialization

---
+++ internal/database/database.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+	dbURL := os.Getenv("DATABASE_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Successfully connected to the database!")
+
+	return db,nil
+
+
+}
+
+func InitDB(db *sql.DB) error {
+	_, err := db.Exec(`
+	      CREATE TABLE IF NOT EXISTS users (
+		     id SERIAL PRIMARY KEY,
+			 email VARCHAR(255) UNIQUE NOT NULL,
+			 password VARCHAR(255) NOT NULL,
+			 created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		  
+		  )`)
+	if err != nil {
+		return err
+	}
+
+	log.Println("Database intialized successfully")
+
+	return nil
+
+}
