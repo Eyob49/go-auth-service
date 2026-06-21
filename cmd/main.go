d04b175750199432c526748c5795e00868f5a5d3
Eyob <eyobk740@gmail.com>
2026-06-21 12:14:05 +0300

    register login route

--- cmd/main.go
+++ cmd/main.go
@@ -23,7 +23,7 @@ func main() {
 	handler := &handlers.AuthHandler{DB: db}
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /register", handler.Register)
-
+    mux.HandleFunc("POST /login", handler.Login)
 	log.Printf("Server is running on http://localhost:8080")
 	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
